4dd4e84a8e168cef389c33deda0b07527ce8f0fc
Maria <maria@MBP-Dmitrij.fritz.box>
2021-07-11 21:36:37 +0300

    refactoring

--- photo-client/src/components/PhotoMainComponet.js
+++ photo-client/src/components/PhotoMainComponet.js
@@ -7,62 +7,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Footer from "./Footer";
 import CategoryPhotoComponent from './CategPhotoComponent'
 
-// class MyCarousel extends Component{
-//     constructor() {
-//         super();
-//         this.state = {
-//             isFetching: false,
-//             photos: []
-//         }
-//         this.handleRenderPhotos= this.handleRenderPhotos.bind(this)
-//
-//     }
-//
-//     componentDidMount() {
-//         this.handleRenderPhotos();
-//
-//     }
-//     handleRenderPhotos = () => {
-//
-//         this.setState({...this.state, isFetching: true});
-//         fetch(adminApi + 'photos')
-//             .then(response => response.json())
-//
-//             .then(result => {
-//                 this.setState({photos: result, isFetching: false})
-//             })
-//             .catch(e => {
-//                 console.log(e);
-//                 this.setState({...this.state, isFetching: false});
-//             });
-//
-//     }
-//
-//
-//     render() {
-//         let showphoto = this.state.photos.map((photo) =>
-//                 <Carousel.Item interval={800}>
-//                     <img className='d-block w-100' id='photomain' src={baseUrl+ photo.image} />
-//                 </Carousel.Item>
-//         )
-//         return(
-//             <React.Fragment>
-//                 <div className='container '>
-//                     <div className='row'>
-//                         <Carousel id='mycarousel'>
-//                             {showphoto}
-//                         </Carousel>
-//                     </div>
-//
-//
-//                 </div>
-//
-//
-//             </React.Fragment>
-//
-//         )
-//     }
-// }
 
 export default class PhotoMain extends Component {
     constructor(props) {
