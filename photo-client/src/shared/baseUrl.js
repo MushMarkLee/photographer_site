11df47613233c8e2fe94243d770f590aa5903cc2
Maria <maria@MBP-Dmitrij.fritz.box>
2021-07-11 21:19:36 +0300

    update global host constants in react

--- photo-client/src/shared/baseUrl.js
+++ photo-client/src/shared/baseUrl.js
@@ -1,6 +1,6 @@
 
-export const baseUrl = 'http://localhost:5000/';
+export const baseUrl = 'http://139.59.157.47:5000/';
 
-export const adminApi = 'http://localhost:5000/admin/';
+export const adminApi = 'http://139.59.157.47:5000/admin/';
 
 export let globalVars = {'token': null}
